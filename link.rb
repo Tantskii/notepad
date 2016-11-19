b8d6f18fe42477d9e046d307356d07d3ac395860
Tantskii <Tihon1997@gmail.com>
2016-11-19 14:08:53 +0300

    class post + ancestors

---
+++ link.rb
@@ -0,0 +1,19 @@
+class Link < Post
+
+  def initialize
+    super #Сопри метод с таким же названием у родительсокго класса
+
+    @url = ''
+  end
+
+  def read_from_console
+
+  end
+
+  def to_strings
+
+  end
+
+
+
+end
